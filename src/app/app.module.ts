984e379e3341e5ba546c4e7e4c57f28b4339da54
Maicon Godinho <maicongodinho1@gmail.com>
2022-01-04 23:10:19 -0300

    feat: add homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { GridModule } from 'carbon-components-angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, GridModule],
   providers: [],
   bootstrap: [AppComponent],
 })
